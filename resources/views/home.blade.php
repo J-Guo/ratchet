15ca0c9d850241971c187d7e637c90f0fcb721ca
Junjie <jguo1495@sydney.edu.au>
2016-02-08 17:13:23 +1100

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.main')
+
+@section('content')
+
+ Hello Worlds
+@stop
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Ratchet API Test</title>
+    @yield("header")
+</head>
+<body>
+
+<div class="container">
+    @yield("content")
+</div>
+
+<!-- Build jQuery -->
+<script src="{{ asset('js/jquery-2.2.0.min.js')}}"></script>
+<!-- Build Stripe -->
+
+@yield("footer")
+
+</body>
+</html>
